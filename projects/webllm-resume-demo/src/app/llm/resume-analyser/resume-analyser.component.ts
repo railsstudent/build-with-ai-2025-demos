abcd5d73aefa3f93ec535cebc98de31da61813d6
Connie Leung <cleung334@gmail.com>
2025-03-17 00:29:03 +0800

    feat: resume analyser

---
+++ projects/webllm-resume-demo/src/app/llm/resume-analyser/resume-analyser.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-analyser',
+  imports: [],
+  templateUrl: './resume-analyser.component.html',
+  styleUrl: './resume-analyser.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ResumeAnalyserComponent {
+
+}
