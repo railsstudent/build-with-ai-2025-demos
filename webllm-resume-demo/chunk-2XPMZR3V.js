b8a4a62e392d9b232d18a039761b0998e37914cb
Connie Leung <cleung334@gmail.com>
2025-04-06 18:28:52 +0800

    chore: deploy to github page

---
+++ webllm-resume-demo/chunk-2XPMZR3V.js
@@ -0,0 +1,1 @@
+import{R as o}from"./chunk-2H5RJ6A3.js";var n=new o("WEB_LLM_CACHE_CONFIG");export{n as a};
--- webllm-resume-demo/chunk-UFW4LOUU.js
+++
@@ -1,1 +0,0 @@
-import{Hb as a,O as i,U as e,ma as r}from"./chunk-7XO67NS5.js";var n=class o{appConfig=e(a);modelList=r(this.appConfig.model_list.map(t=>({id:t.model_id,url:t.model})));static \u0275fac=function(c){return new(c||o)};static \u0275prov=i({token:o,factory:o.\u0275fac,providedIn:"root"})};export{n as a};
